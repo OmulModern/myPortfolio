86bc824eecd23655cb687e868ea4bd939e1d8a7e
OmulModern <contact.omulmodern@gmail.com>
2020-05-20 23:10:18 +0300

    perfect animations on scroll and better responsive

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 wow = new WOW({
   boxClass: 'wow',      // default
   animateClass: 'animated', // default
-  offset: 100,          // default
+  offset: 150,          // default
   mobile: true,       // default
   live: true        // default
 })
