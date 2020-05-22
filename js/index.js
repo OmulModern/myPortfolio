96b1049ae90da22be01cd9d2e6199b41d2847ddd
OmulModern <contact.omulmodern@gmail.com>
2020-05-22 11:42:32 +0300

    Made magnific popup Work Awesome

--- js/index.js
+++ js/index.js
@@ -44,4 +44,28 @@ wow = new WOW({
 
 wow.init();
 
+// Magnific Popup
+$(document).ready(function () {
+  $(".video").magnificPopup({
+    type: "iframe",
+    iframe: {
+      markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+              '</div>',
+      
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+
+          src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+        },
+      },
+
+      srcAction: 'iframe_src',
+    }
+  })
+});
+
 
