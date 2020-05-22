9b854cb3905cd19d86f2778883e005f9bb58b332
OmulModern <contact.omulmodern@gmail.com>
2020-05-22 13:35:03 +0300

    making js files more readeable

--- js/en.index.js
+++ js/en.index.js
@@ -1,6 +1,44 @@
 
 $(document).ready(function () {
 
+  // Load particlesJS
+  particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('particles.json loaded');
+  });
+
+  // LOAD TYPED JS IN HERO SECTION
+  var typed = new Typed('.typed', {
+    strings: ["vegan.", "web developer.", "dreamer.", "multidisciplinary.", "vizionary."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 300,
+    showCursor: false,
+    contentType: null,
+    backSpeed: 30,
+  });
+
+  // Scroll to About Page
+  $('.js-scroll-to-about').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js-about').offset().top
+    }, 800);
+  });
+
+  // Scroll to top
+  $('.js-scroll-to-top').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#top').offset().top
+    }, 800);
+  });
+
+  // TILT JS | Projects Images ANIMATION ON HOVER
+  $('.thumbnail').tilt({
+    maxTilt: 2.5,
+    glare: true,
+    maxGlare: .2,
+    reset: false
+  });
+
   // Magnific Popup
   $(".video").magnificPopup({
     type: "iframe",
@@ -18,47 +56,8 @@ $(document).ready(function () {
           src: 'https://www.youtube.com/embed/%id%?autoplay=1'
         },
       },
-
       srcAction: 'iframe_src',
     }
-  });
-
-  // TILT JS | Projects Images ANIMATION ON HOVER
-  $('.thumbnail').tilt({
-    maxTilt: 2.5,
-    glare: true,
-    maxGlare: .2,
-    reset: false
-  });
-
-  // Scroll to About Page
-  $('.js-scroll-to-about').click(function () {
-    $('html, body').animate({
-      scrollTop: $('.js-about').offset().top
-    }, 800);
-  });
-
-  // Scroll to top
-  $('.js-scroll-to-top').click(function () {
-    $('html, body').animate({
-      scrollTop: $('#top').offset().top
-    }, 800);
-  });
-
-  // LOAD TYPED JS IN HERO SECTION
-  var typed = new Typed('.typed', {
-    strings: ["vegan.", "web developer.", "dreamer.", "multidisciplinary.", "vizionary."],
-    typeSpeed: 70,
-    loop: true,
-    startDelay: 300,
-    showCursor: false,
-    contentType: null,
-    backSpeed: 30,
-  });
-
-  // Load particlesJS
-  particlesJS.load('particles-js', 'particles.json', function () {
-      console.log('particles.json loaded');
   });
 
   // Activate animations on scroll
--- js/index.js
+++ js/index.js
@@ -1,26 +1,20 @@
-// LOAD PARTICLES
-particlesJS.load('particles-js', 'particles.json', function () {
-    console.log('particles.json loaded');
-});
-
-// LOAD TYPED JS IN HERO SECTION
-var typed = new Typed('.typed', {
-  strings: ["web developer.", "multidisciplinar.", "vizionar.", "vegan."],
-  typeSpeed: 70,
-  loop: true,
-  startDelay: 300,
-  showCursor: false,
-  contentType: null,
-  backSpeed: 30,
-});
 
 $(document).ready(function () {
-  // TILT JS | Projects Images ANIMATION ON HOVER
-  $('.thumbnail').tilt({
-    maxTilt: 2.5,
-    glare: true,
-    maxGlare: .2,
-    reset: false
+
+  // Load particlesJS
+  particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('particles.json loaded');
+  });
+
+  // LOAD TYPED JS IN HERO SECTION
+  var typed = new Typed('.typed', {
+    strings: ["vegan.", "web developer.", "dreamer.", "multidisciplinary.", "vizionary."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 300,
+    showCursor: false,
+    contentType: null,
+    backSpeed: 30,
   });
 
   // Scroll to About Page
@@ -37,30 +31,23 @@ $(document).ready(function () {
     }, 800);
   });
 
-});
+  // TILT JS | Projects Images ANIMATION ON HOVER
+  $('.thumbnail').tilt({
+    maxTilt: 2.5,
+    glare: true,
+    maxGlare: .2,
+    reset: false
+  });
 
-// Activate animations on scroll
-wow = new WOW({
-  boxClass: 'wow',      // default
-  animateClass: 'animate__animated', // default
-  offset: 150,          // default
-  mobile: true,       // default
-  live: false,        // default
-  resetAnimation: true,
-})
-
-wow.init();
-
-// Magnific Popup
-$(document).ready(function () {
+  // Magnific Popup
   $(".video").magnificPopup({
     type: "iframe",
     iframe: {
       markup: '<div class="mfp-iframe-scaler">' +
-                '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-              '</div>',
-      
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '</div>',
+
       patterns: {
         youtube: {
           index: 'youtube.com/',
@@ -69,10 +56,22 @@ $(document).ready(function () {
           src: 'https://www.youtube.com/embed/%id%?autoplay=1'
         },
       },
-
       srcAction: 'iframe_src',
     }
+  });
+
+  // Activate animations on scroll
+  wow = new WOW({
+    boxClass: 'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset: 150,          // default
+    mobile: true,       // default
+    live: false,        // default
+    resetAnimation: true,
   })
+
+  wow.init();
+
 });
 
 
